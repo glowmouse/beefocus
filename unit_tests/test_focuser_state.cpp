5ad0f8fa1ca8a601e73dd13659bef2200e9fa886
Andrew Brownbill <andrew.brownbill@gmail.com>
2018-12-14 21:22:50 -0500

    Refactor the focuser state test so elements that will be common to multiple tests are in separate functions

--- unit_tests/test_focuser_state.cpp
+++ unit_tests/test_focuser_state.cpp
@@ -3,18 +3,16 @@
 #include "focuser_state.h"
 #include "test_mocks.h"
 
-TEST( COMMAND_PARSER, init_Focuser )
+std::unique_ptr<FOCUSER_STATE> make_focuser( 
+  const NetMockSimpleTimed::TimedStringEvents& wifiIn,
+  NetMockSimpleTimed* &net_interface
+)
 {
-  NetMockSimpleTimed::TimedStringEvents input = {
-    { 0, "status" },      // status @ Time 0
-    { 50, "pstatus" }    // status @ Time 1000 ms
-  };
-
-  std::unique_ptr<NetMockSimpleTimed> wifi( new NetMockSimpleTimed( input ));
+  std::unique_ptr<NetMockSimpleTimed> wifi( new NetMockSimpleTimed( wifiIn ));
   std::unique_ptr<DebugInterfaceIgnoreMock> debug( new DebugInterfaceIgnoreMock);
   std::unique_ptr<HWMockTimed> hardware( new HWMockTimed );
-
-  auto wifiAlias = wifi.get();
+  
+  net_interface = wifi.get();
 
   auto focuser = std::unique_ptr<FOCUSER_STATE>(
      new FOCUSER_STATE(
@@ -23,15 +21,44 @@ TEST( COMMAND_PARSER, init_Focuser )
         std::move(debug) )
   );
 
+  return focuser;
+}
+
+/// @brief Simulate the focuser
+///
+/// @param[out] Focuser  - A pointer to the focuser.
+/// @param[in] wifiAlisa - A pointer to the WIFI/ Network Mock
+/// @param[in] end_time  - How long (in MS) to run the focuser for.
+/// 
+void simulateFocuser( 
+  FOCUSER_STATE* focuser,
+  NetMockSimpleTimed* wifiAlias,
+  unsigned int endTime
+)
+{
   unsigned int time = 0;
   unsigned int lastMockAdvanceTime = 0;
-  while ( time < 100000)
+  while ( time < endTime*1000 )
   {
     time = time + focuser->loop();
     unsigned int mockAdvanceTime = time/1000;
     wifiAlias->advanceTime( mockAdvanceTime - lastMockAdvanceTime );
     lastMockAdvanceTime = mockAdvanceTime;
   }
+}
+
+/// @brief Init the focuser and pass in some status commands
+///
+TEST( COMMAND_PARSER, init_Focuser )
+{
+  NetMockSimpleTimed::TimedStringEvents input = {
+    { 0, "status" },      // status @ Time 0
+    { 50, "pstatus" }     // status @ Time 1000 ms
+  };
+
+  NetMockSimpleTimed* wifiAlias;
+  auto focuser = make_focuser( input, wifiAlias ); 
+  simulateFocuser( focuser.get(), wifiAlias, 1000 );
 
   NetMockSimpleTimed::TimedStringEvents golden = {
     {  0, "# Got: status" },
